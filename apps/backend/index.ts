699ef4d390c723cf7708d6b1cbc7c35d0ac6ca32
harkirat <harkirat96@gmail.com>
2025-02-16 20:37:27 +0530

    Added be, fe, s

---
+++ apps/backend/index.ts
@@ -0,0 +1,40 @@
+import express from "express";
+import { prismaClient } from "db/client";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/users", (req, res) => {
+  prismaClient.user.findMany()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+      res.status(500).json({ error: err.message });
+    });
+})
+
+app.post("/user", (req, res) => {
+  const { username, password } = req.body;
+  
+  if (!username || !password) {
+    res.status(400).json({ error: "Username and password are required" });
+    return
+  }
+
+  prismaClient.user.create({
+    data: {
+      username,
+      password
+    }
+  })
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({ error: err.message });
+    });
+})
+
+app.listen(8080);
