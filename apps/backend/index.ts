9e9204134856c9739cddd10d9cad35b242099165
Yash Wankhade <yashwankhade5@gmail.com>
2025-04-11 13:34:28 +0530

    some

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -11,7 +11,7 @@ app.get("/users", (req, res) => {
       res.json({users});
     })
     .catch(err => {
-      res.status(400).json({ error: err.message });
+      res.status(500).json({ error: err.message });
     });
 })
 
